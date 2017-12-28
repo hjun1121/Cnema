91bed45908b03da0b96db20f9ba278740a1c9539
Heeseong <hsung0823@hanmail.net>
2017-12-28 15:13:45 +0900

    mypage

--- src/main/java/com/cnema/c1/MemberController.java
+++ src/main/java/com/cnema/c1/MemberController.java
@@ -1,24 +1,33 @@
 package com.cnema.c1;
 
+
 import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.cnema.member.MemberDTO;
+import com.cnema.member.MemberService;
+
 
 @Controller
 @RequestMapping(value="/member/**")
 public class MemberController {
+	@Inject
+	private MemberService memberService;
 	
 	@RequestMapping(value="myPageView", method=RequestMethod.GET)
 	public void selectOne(String id){
-		System.out.println("dododo");
-		/*try {
-			memberService.selectOne(id);
+		try {
+			MemberDTO memberDTO = memberService.selectOne(id);
+			System.out.println(memberDTO.getPw());
 		} catch (Exception e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}*/
+		}
+		
+		
 	}
 	
 }
