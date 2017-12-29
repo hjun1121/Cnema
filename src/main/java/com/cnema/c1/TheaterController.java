f4c817edad31ee1f3432df6b9d65c9a8ee91c1a2
kim <hjun1121@naver.com>
2017-12-29 14:22:23 +0900

    theater

---
+++ src/main/java/com/cnema/c1/TheaterController.java
@@ -0,0 +1,12 @@
+package com.cnema.c1;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value="theater/*")
+public class TheaterController {
+	
+	
+	
+}
