dc4580455d49882255939d3e6a7527e8a3adadf2
heeseong <hsung0823@hanmail.net>
2018-01-01 00:58:16 +0900

    commit

--- src/main/java/com/cnema/c1/MovieController.java
+++ src/main/java/com/cnema/c1/MovieController.java
@@ -4,17 +4,30 @@ import javax.inject.Inject;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestMethod;
+=======
+>>>>>>> parent of 2ba8889... 씨발럼
 import org.springframework.web.servlet.ModelAndView;
 <<<<<<< HEAD
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+<<<<<<< HEAD
 import com.cnema.coupon.MyCouponDTO;
 import com.cnema.member.MemberDTO;
 import com.cnema.movie.MovieDTO;
 =======
 >>>>>>> 7fd5c7a548766b0d0d6c04021eccbf3f8e3fe80a
 import com.cnema.movie.MovieService;
+=======
+import com.cnema.member.MemberDTO;
+import com.cnema.movie.MovieDTO;
+=======
+
+>>>>>>> parent of 41279c7... Hi
+import com.cnema.movie.MovieService;
+import com.cnema.util.ListData;
+>>>>>>> parent of 2ba8889... 씨발럼
 
 @Controller
 @RequestMapping(value = "/movie/*")
@@ -23,7 +36,9 @@ public class MovieController {
 	@Inject
 	private MovieService movieService;
 	
+<<<<<<< HEAD
 	//selectOne
+<<<<<<< HEAD
 	@RequestMapping(value="movie_view")
 	public ModelAndView selectOne(String id,ModelAndView mv,RedirectAttributes rd) throws Exception {
 		MovieDTO movieDTO = null;
@@ -35,6 +50,39 @@ public class MovieController {
 		mv.setViewName("movie/movieView");
 
 
+=======
+	@RequestMapping(value="noticeView")
+	public ModelAndView selectOne(String id,ModelAndView mv,RedirectAttributes rd) throws Exception {
+		MovieDTO movieDTO = null;
+		
+		
+		MemberDTO memberDTO = null;
+		id="hseong";
+		try {
+			//memberDTO = memberService.selectOne(id);
+			System.out.println(memberDTO.getName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		if(memberDTO != null){
+			mv.addObject("myInfo",memberDTO);
+			mv.setViewName("member/myPageView");
+		}else{
+			rd.addFlashAttribute("message","로그인이 필요합니다.");
+			mv.setViewName("redirect:../home");
+		}
+		return mv;
+	}
+	
+=======
+	
+	//selectList
+	@RequestMapping(value = "movieList")
+	public ModelAndView selectList(ModelAndView mv) throws Exception {
+		mv = movieService.selectList();
+		
+>>>>>>> parent of 2ba8889... 씨발럼
 		return mv;
 	}
 	
@@ -58,7 +106,11 @@ public class MovieController {
 //		return mv;
 //	}
 	
+<<<<<<< HEAD
 
+=======
+>>>>>>> parent of 41279c7... Hi
+>>>>>>> parent of 2ba8889... 씨발럼
 	//insert
 	
 	//delete
--- src/main/java/com/cnema/movie/MovieService.java
+++ src/main/java/com/cnema/movie/MovieService.java
@@ -9,12 +9,34 @@ import org.springframework.stereotype.Service;
 @Service
 public class MovieService {
 
+<<<<<<< HEAD
 	@Inject
 	private MovieDAO movieDAO;
 	
 	public MovieDTO selectOne(int num) throws Exception{
 		return movieDAO.selectOne(num);
 
+=======
+<<<<<<< HEAD
+
+	@Inject
+	private MovieDAO movieDAO;
+//	@Inject
+//	private FileSaver fileSaver;
+=======
+	@Inject
+	private MovieDAO movieDAO;
+	@Inject
+	private FileSaver fileSaver;
+>>>>>>> parent of 41279c7... Hi
+
+	public ModelAndView selectList() throws Exception {
+		ModelAndView mv = new ModelAndView();
+		List<MovieDTO> ar = movieDAO.selectList();
+		mv.addObject("list", ar);
+		mv.setViewName("movie/movie_chart");
+		return mv;
+>>>>>>> parent of 2ba8889... 씨발럼
 	}
 
 }
