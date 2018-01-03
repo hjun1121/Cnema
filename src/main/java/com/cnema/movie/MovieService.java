7e8842eba25cf1c7dc565826fc13d9b11f30a7ad
ssin226 <ssin226@naver.com>
2018-01-03 12:22:04 +0900

    movie_insert

--- src/main/java/com/cnema/movie/MovieService.java
+++ src/main/java/com/cnema/movie/MovieService.java
@@ -3,8 +3,15 @@ package com.cnema.movie;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.mock.web.portlet.MockActionRequest;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.cnema.member.MemberDTO;
+import com.cnema.util.FileSaver;
 
 
 @Service
@@ -13,17 +20,27 @@ public class MovieService {
 	@Inject
 	private MovieDAO movieDAO;
 	
+	@Inject
+	private FileSaver fileSaver;
+	
+	
+	public List<MovieDTO> movieList() throws Exception {
+		return movieDAO.movieList();
+	}
+	
 	public MovieDTO selectOne(int num) throws Exception{
 		return movieDAO.selectOne(num);
 
 	}
 
-	public ModelAndView selectList() throws Exception {
-		ModelAndView mv = new ModelAndView();
-		return mv;
+	public int insert(MovieDTO movieDTO, HttpSession session) throws Exception {
+		MultipartFile file = movieDTO.getFile();
+		String name = fileSaver.fileSave(file, session, "poster");
+		movieDTO.setFileName(name);
+		movieDTO.setOriName(file.getOriginalFilename());
+		int result = movieDAO.insert(movieDTO);
+		
+		return result;
 	}
-
-	public List<MovieDTO> movieList() throws Exception{
-		return movieDAO.movieList();
-	}
+	
 }
--- src/main/webapp/WEB-INF/views/movie/movie_chart.jsp
+++ src/main/webapp/WEB-INF/views/movie/movie_chart.jsp
@@ -8,7 +8,19 @@
 <title>Insert title here</title>
 </head>
 <body>
-
+<h1>영화 insert</h1>
+<form action="movie_chart" method="post" name="frm" enctype="multipart/form-data">
+		<p>name <input type="text" id="movie_name" name="movie_name"></p>
+		포스터<input type="file" name="file">
+		<p>티저영상<input type="text" id="teaser_url" name="teaser_url"></p>
+		<p>개봉일<input type="date" id="open_date" name="open_date"></p>
+		<p>상영시간<input type="text" id="run_time" name="run_time"></p>
+		<p>장르<input type="text" id="type" name="type"></p>
+		<p>영화설명<input type="text" id="contents" name="contents"></p>
+		<p>나이제한<input type="text" id="age_limit" name="age_limit"></p>
+		<input type="submit" id="insertBtn" value="insert">
+</form>
+<br>
 <h1>무비차트</h1>
 <hr>
 <table>
