311e83399ecc066b857bdc1dce9501e7d6bb2b44
SenaPark <psn1091@naver.com>
2018-01-08 10:58:24 +0900

    setting2

---
+++ src/main/java/com/cnema/event/EventJoinDTO.java
@@ -0,0 +1,25 @@
+package com.cnema.event;
+
+public class EventJoinDTO {
+	private int num;
+	private String id;
+	private int type;
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public int getType() {
+		return type;
+	}
+	public void setType(int type) {
+		this.type = type;
+	}
+}
