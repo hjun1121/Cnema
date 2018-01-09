c76eff68bdc9a375a9656d6df07ac7bf6b5f3591
SenaPark <psn1091@naver.com>
2018-01-09 15:19:20 +0900

    eventUpdate

--- src/main/java/com/cnema/c1/EventController.java
+++ src/main/java/com/cnema/c1/EventController.java
@@ -4,11 +4,13 @@ import javax.inject.Inject;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.cnema.board.BoardDTO;
 import com.cnema.event.EventDTO;
 import com.cnema.event.EventService;
 import com.cnema.util.ListData;
@@ -48,10 +50,71 @@ public class EventController {
 			
 			return "redirect:./eventList";
 		}
-	
 		
+		//View페이지
+		@RequestMapping(value="eventView")
+		public ModelAndView selectOne(ModelAndView mv, int num, RedirectAttributes rd) throws Exception{
+			BoardDTO eventDTO = null;
+			
+				eventDTO = eventService.selectOne(num);
+			
+			
+			if(eventDTO != null){
+				mv.addObject("view", eventDTO);
+				mv.setViewName("event/eventView");
+			}else{
+				rd.addFlashAttribute("message","업습니다.");
+				mv.setViewName("redirect:./eventList");
+			}
+			
+			return mv;
+		}
 		
+		//delete
+		@RequestMapping(value="eventDelete")
+		public String delete(int num, RedirectAttributes rd)throws Exception{
+		int result = 0;
+					
+		result = eventService.delete(num);
+					
+		String message = "fail";
+		if(result>0){
+			message = "success";
+		}
+		rd.addFlashAttribute("message", message);
+					
+		return "redirect:./eventList";
+		}
 		
+		//Update Form 들어가기
+		@RequestMapping(value="eventUpdate",method=RequestMethod.GET)
+		public String update(Model model, int num) throws Exception{
+			BoardDTO boardDTO = null;
+			try {
+				boardDTO = eventService.selectOne(num);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			model.addAttribute("view", boardDTO);
+			return "event/eventUpdate";
+		}
+		
+		//update --> DB 
+		@RequestMapping(value="eventUpdate",method=RequestMethod.POST)
+		public String update(EventDTO eventDTO,HttpSession session ,RedirectAttributes rd) throws Exception{
+			int result = 0;
+			
+				result = eventService.update(eventDTO,session);
+			
+			String message = "fail";
+			if(result>0){
+				message = "success";
+			}
+			rd.addFlashAttribute("message", message);
+			
+			return "redirect:./eventList";
+		}
 		
 		
 }//end
--- src/main/webapp/WEB-INF/views/event/eventUpdate.jsp
+++ src/main/webapp/WEB-INF/views/event/eventUpdate.jsp
@@ -5,7 +5,69 @@
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<script src="https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script type="text/javascript" src="../resources/SE2/js/HuskyEZCreator.js"></script>
 <title>Insert title here</title>
+<script type="text/javascript">
+	$(function(){
+		//SmartEditor start
+		//전역변수선언
+    var editor_object = [];
+     
+    nhn.husky.EZCreator.createInIFrame({
+        oAppRef: editor_object,
+        //textarea ID
+        elPlaceHolder: "contents",
+        sSkinURI: "../resources/SE2/SmartEditor2Skin.html", 
+        htParams : {
+            // 툴바 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseToolbar : true,             
+            // 입력창 크기 조절바 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseVerticalResizer : true,     
+            // 모드 탭(Editor | HTML | TEXT) 사용 여부 (true:사용/ false:사용하지 않음)
+            bUseModeChanger : true, 
+        }
+    });
+     
+    //전송버튼 클릭이벤트
+    $("#updateBtn").click(function(){
+        //id가 smarteditor인 textarea에 에디터에서 대입
+        editor_object.getById["contents"].exec("UPDATE_CONTENTS_FIELD", []);
+         
+        // 이부분에 에디터 validation 검증
+         
+        //폼 submit
+        $("#frm").submit();
+    })
+		
+		//CKEDITOR.replace( 'contents' );
+		
+	
+			 
+		 //쌤 방식
+	/* 	var index = 0;
+		var count = 0;
+		$("#add").click(function(){
+			if(count < 5){
+				var s = '<div id="d'+index+'">';
+				s = s+'<input type="file" name="f1"><span class="del" title="d'+index+'">X</span></div>';
+				$("#files").append(s);
+				count++;
+				index++;
+			}else{
+				alert("fail");
+			}
+		})
+		
+		$("#files").on("click",".del",function(){
+			var id = $(this).attr("title");
+			$("#"+id).remove();
+			index--;
+		}); */
+		
+	});
+</script>
 </head>
 <body>
 <h2>이벤트 수정하기</h2>
@@ -19,13 +81,13 @@
     </em>
 </div>
 <div class="eventViewContents">
-<textarea name="contents" rows="10" cols="30"> ${view.contents}</textarea>
+<textarea id="contents" name="contents" rows="10" cols="30"> ${view.contents}</textarea>
 
 </div>
 
 <!--  파일수정하기  -->
 <div class="eventViewFile">
-파일수정
+파일수정 : ${view.oriName }
 <input type="file" name="file">
 </div>
 
