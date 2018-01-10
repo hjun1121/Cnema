2c05f2f52fa71d1615b6cfbe00cc235f178af5cf
kim <hjun1121@naver.com>
2018-01-10 14:05:39 +0900

    qr ver(18.1.10)

--- src/main/java/com/cnema/c1/TheaterController.java
+++ src/main/java/com/cnema/c1/TheaterController.java
@@ -11,7 +11,11 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.cnema.movie.MovieDTO;
 import com.cnema.movie.MovieService;
+import com.cnema.reserve.ReserveDTO;
 import com.cnema.theater.DayDTO;
+import com.cnema.theater.ScheduleDTO;
+import com.cnema.theater.ScheduleService;
+import com.cnema.theater.ScreenDTO;
 import com.cnema.theater.TheaterDTO;
 import com.cnema.theater.TheaterService;
 
@@ -23,6 +27,31 @@ public class TheaterController {
 	private TheaterService theaterService;
 	@Inject
 	private MovieService movieService;
+	@Inject
+	private ScheduleService scheduleService;
+	
+	@RequestMapping(value="quickReserve2", method=RequestMethod.GET)
+	public void quickReserve2(Model model, ReserveDTO reserveDTO){
+		MovieDTO movieDTO = null;
+		TheaterDTO theaterDTO = null;
+		ScheduleDTO scheduleDTO = null;
+		ScreenDTO screenDTO = null;
+		try {
+			movieDTO = movieService.selectOne(reserveDTO.getMovie_num());
+			theaterDTO = theaterService.selectOne(reserveDTO.getTheater_num());
+			scheduleDTO = scheduleService.scheduleOne(reserveDTO.getSchedule_num());
+			screenDTO = scheduleService.screenOne(scheduleDTO.getScreen_num());			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		model.addAttribute("screenDTO", screenDTO);
+		model.addAttribute("day", reserveDTO.getDay_num());
+		model.addAttribute("theater", theaterDTO);
+		model.addAttribute("movie", movieDTO);
+		model.addAttribute("reserve", reserveDTO);
+		
+	}
 	
 	@RequestMapping(value="quickReserve", method=RequestMethod.GET)
 	public void quickReserve(Model model, String area, String kind){
