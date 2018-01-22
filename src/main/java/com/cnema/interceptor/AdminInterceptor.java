f0677e8df8c4b1472fd20172e8ac272e19bf4c15
kim <hjun1121@naver.com>
2018-01-22 13:57:53 +0900

    admin

--- src/main/java/com/cnema/interceptor/AdminInterceptor.java
+++ src/main/java/com/cnema/interceptor/AdminInterceptor.java
@@ -23,14 +23,18 @@ public class AdminInterceptor extends HandlerInterceptorAdapter {
 			ModelAndView modelAndView) throws Exception {
 		HttpSession session = request.getSession();
 		MemberDTO memberDTO = (MemberDTO)session.getAttribute("member");
+		String path = request.getServletPath();
+		path = path.substring(1);
 		
 		if(memberDTO==null){
 			String message = "로그인이 필요한 서비스입니다.";
 			modelAndView.setViewName("common/login");
+			modelAndView.addObject("path", path);
 			modelAndView.addObject("message", message);
 		}else if(memberDTO.getType() != 20){
 			String message = "접근 권한이 없습니다.";
 			modelAndView.addObject("message", message);
+			modelAndView.setViewName("common/result");
 		}
 
 		
